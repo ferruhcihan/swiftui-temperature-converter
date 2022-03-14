2b8260cfb262923f86b261c0debdbb67c0ad19b1
Ferruh <ferruh.cihan@westerops.com>
2022-03-14 22:13:34 +0300

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by yoda on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
