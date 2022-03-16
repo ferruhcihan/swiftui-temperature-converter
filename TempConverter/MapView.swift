e76def19233ea5a8cd08a75511468658a0134c66
Ferruh <ferruhcihan20@gmail.com>
2022-03-16 20:16:03 +0300

    feat:add MapView page

---
+++ TempConverter/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  TempConverter
+//
+//  Created by yoda on 16.03.2022.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
